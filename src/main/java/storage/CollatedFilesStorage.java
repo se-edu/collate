b76d1d6eaa533b15d7f9a287ef2b188f76f36ba3
Sebastian Quek <sebq13@gmail.com>
2015-06-25 17:08:48 +0800

    [#19] Implemented CollatedFilesStorage.java

--- src/main/java/storage/CollatedFilesStorage.java
+++ src/main/java/storage/CollatedFilesStorage.java
@@ -1,5 +1,34 @@
 package main.java.storage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
 public class CollatedFilesStorage {
 
+    private static final String DEFAULT_SAVE_DIRECTORY = "collated";
+
+    public CollatedFilesStorage() {
+        createSaveDirectory();
+    }
+
+    private void createSaveDirectory() {
+        File dir = new File(DEFAULT_SAVE_DIRECTORY);
+
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+    }
+
+    public void addCollatedFile(String fileName, ArrayList<String> collatedLines) {
+        try (PrintWriter writer = new PrintWriter(DEFAULT_SAVE_DIRECTORY + "/" +
+                                                  fileName + ".md")) {
+            for (String line : collatedLines) {
+                writer.println(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 }
