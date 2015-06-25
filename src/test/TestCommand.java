6d4084fd5004c00398feca44407e132191af465a
Sebastian Quek <sebq13@gmail.com>
2015-06-25 12:53:31 +0800

    [#18] Added tests for Command.java

---
+++ src/test/TestCommand.java
@@ -0,0 +1,28 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import main.java.logic.Command;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCommand {
+    
+    private Command command;
+    
+    @Before
+    public void init() {
+        command = new Command("collate /src");
+    }
+
+    @Test
+    public void testGetCommandType() {
+        assertEquals("COLLATE", command.getCommandType().toString());
+    }
+
+    @Test
+    public void testGetArguments() {
+        assertEquals("/src", command.getArguments());
+    }
+
+}
