a30d4f9689272e319351a25e824d114e86a95f3a
Sebastian Quek <sebq13@gmail.com>
2015-06-25 15:30:23 +0800

    [#1 #2 #18] Logic now detects "@author" tags to demarcate code. "collate from <DIR>" collates all files in the directory and sub directories. The markdown output is now shown in the ListView.

--- src/main/java/logic/Logic.java
+++ src/main/java/logic/Logic.java
@@ -1,18 +1,34 @@
 package main.java.logic;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.scene.input.KeyCode;
 import main.java.gui.CommandBarController;
 import main.java.gui.OverviewLayoutController;
+import main.java.logic.data.Author;
+import main.java.logic.data.CodeSnippet;
 import main.java.logic.parser.CommandParser;
 
 public class Logic {
-    private static CommandParser commandParser;
-    private static final String JAVA_AUTHOR_TAG = "@contributor";
 
-    public Logic() {
+    private static Logger logger;
+    private static CommandParser commandParser;
+    private static HashMap<String, Author> authors;
+
+    private static final String LOG_TAG = "Logic";
+    private static final int INITIAL_NUM_CONTRIBUTORS = 5;
+    private static final String JAVA_AUTHOR_TAG = "@author";
+
+    static {
+        logger = Logger.getLogger(LOG_TAG);
         commandParser = new CommandParser();
+        authors = new HashMap<String, Author>(INITIAL_NUM_CONTRIBUTORS);
     }
 
     public static void handleKeyPress(CommandBarController commandBar,
@@ -24,11 +40,19 @@ public class Logic {
         }
     }
 
+
+    // ================================================================
+    // Private methods
+    // ================================================================
+
     private static void handleEnterPress(String userInput) {
         Command command = new Command(userInput);
         executeCommand(command);
-        OverviewLayoutController.updateOverviewDisplay(command.getCommandType()
-                                                              .toString());
+        for (Author author : authors.values()) {
+            for (CodeSnippet codeSnippet : author.getCodeSnippets()) {
+                OverviewLayoutController.updateOverviewDisplay(codeSnippet.toString());
+            }
+        }
     }
 
     private static void executeCommand(Command command) {
@@ -37,13 +61,21 @@ public class Logic {
                 handleCollate(command.getArguments());
                 break;
             case INVALID :
+            default :
                 break;
         }
     }
 
+
+    // ================================================================
+    // Collate methods
+    // ================================================================
+
     private static void handleCollate(String arguments) {
         String directory = commandParser.getDirectory(arguments);
-        traverseDirectory(directory);
+        if (directory != null) {
+            traverseDirectory(directory);
+        }
     }
 
     private static void traverseDirectory(String directory) {
@@ -55,15 +87,49 @@ public class Logic {
         for (File file : folder.listFiles()) {
             if (file.isDirectory()) {
                 traverseDirectory(file);
-            } else if (getFileExtension(file) == "java") {
+            } else if (getFileExtension(file).equals("java")) {
+                logger.log(Level.INFO, "Found file: " + file);
                 collateFile(file);
             }
         }
     }
 
     private static void collateFile(File file) {
-        // TODO Auto-generated method stub
-        
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line = reader.readLine();
+            Author currentAuthor = null;
+            CodeSnippet currentSnippet = null;
+
+            while (line != null) {
+
+                if (line.contains(JAVA_AUTHOR_TAG)) {
+                    String authorName = findAuthorName(line);
+                    logger.log(Level.INFO, "Found author: " + authorName);
+
+                    if (!authors.containsKey(authorName)) {
+                        currentAuthor = new Author(authorName);
+                        authors.put(authorName, currentAuthor);
+                    } else {
+                        currentAuthor = authors.get(authorName);
+                    }
+
+                    currentSnippet = new CodeSnippet(currentAuthor,
+                                                     file.getPath(),
+                                                     "java");
+                } else if (currentSnippet != null) {
+                    currentSnippet.addLine(line);
+                }
+
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String findAuthorName(String line) {
+        String[] split = line.split(JAVA_AUTHOR_TAG);
+        return split[1].trim();
     }
 
     private static String getFileExtension(File file) {
@@ -71,6 +137,6 @@ public class Logic {
         if (idxLastPeriod != -1) {
             return file.getName().substring(idxLastPeriod + 1);
         }
-        return "";
+        return null;
     }
 }
