c3c5ecd5e5031d35ceecafd4baad7b9567775995
Sebastian Quek <sebq13@gmail.com>
2015-06-25 13:44:52 +0800

    [#17] Added CommandParser

--- src/main/java/logic/parser/CommandParser.java
+++ src/main/java/logic/parser/CommandParser.java
@@ -1,5 +1,27 @@
 package main.java.logic.parser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class CommandParser {
+    
+    private static final String STRING_ONE_SPACE = " ";
+    
+    public CommandParser() {
+    }
+
+    public String getDirectory(String arguments) {
+        ArrayList<String> argArray = splitArguments(arguments);
+        if (argArray.contains("from")) {
+            int directoryIndex = argArray.indexOf("from") + 1;
+            return argArray.get(directoryIndex);
+        }
+        return null;
+    }
+    
+    private ArrayList<String> splitArguments(String arguments) {
+        String[] strArray = arguments.trim().split(STRING_ONE_SPACE);
+        return new ArrayList<String>(Arrays.asList(strArray));
+    }
 
 }
