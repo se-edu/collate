1b9e1b6fd771de5d27a4bd81608bd242b754f121
Sebastian Quek <sebq13@gmail.com>
2015-07-23 22:47:50 +0800

    Refactored and added comments for CommandParser

--- src/main/java/backend/CommandParser.java
+++ src/main/java/backend/CommandParser.java
@@ -3,12 +3,25 @@ package main.java.backend;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.StringJoiner;
 
+/**
+ * CommandParser parses user's input to create Command objects that have the
+ * appropriate fields initialised. For example, the "view" command requires the
+ * authorName field to be initialised.
+ * 
+ * @author Sebastian Quek
+ *
+ */
 public class CommandParser {
-
-    private static final String EMPTY_STRING = "";
+    
     private static final int POSITION_PARAM_COMMAND = 0;
     private static final int POSITION_FIRST_PARAM_ARGUMENT = 1;
+
+    private static final String STRING_BACKSLASH = "\\";
+    private static final String STRING_FORWARD_SLASH = "/";
+    private static final String STRING_DOUBLE_INVERTED_COMMA = "\"";
+    private static final String STRING_EMPTY = "";
     private static final String STRING_ONE_SPACE = " ";
 
     private static final String USER_COMMAND_COLLATE = "collate";
@@ -30,26 +43,25 @@ public class CommandParser {
         ArrayList<String> arguments = getUserArguments(parameters);
 
         switch (userCommand.toLowerCase()) {
+
             case USER_COMMAND_COLLATE :
                 command = initCollateCommand(arguments);
                 break;
+
             case USER_COMMAND_VIEW :
                 command = initViewCommand(arguments);
                 break;
+
             case USER_COMMAND_SUMMARY :
                 command = initSummaryCommand(arguments);
                 break;
+
             default :
                 command = initInvalidCommand();
         }
 
         return command;
     }
-
-
-    // ================================================================
-    // Private methods
-    // ================================================================
 
     private ArrayList<String> splitString(String arguments, String delimiter) {
         String[] strArray = arguments.trim().split(delimiter);
@@ -85,6 +97,18 @@ public class CommandParser {
         }
     }
 
+    /**
+     * A valid collate command requires the following:
+     * 
+     * 1) The input directory exists
+     * 2) If the "include" keyword is specified, it must be followed by words
+     * that indicate file types
+     * 
+     * @param arguments
+     * @param directory
+     * @param fileTypes
+     * 
+     */
     private boolean isValidCollateCommand(ArrayList<String> arguments,
                                           String directory,
                                           ArrayList<String> fileTypes) {
@@ -95,25 +119,50 @@ public class CommandParser {
 
     private String findDirectory(ArrayList<String> arguments) {
         if (arguments.contains(KEYWORD_DIRECTORY)) {
-            int directoryIndex = arguments.indexOf(KEYWORD_DIRECTORY) + 1; 
+            int directoryIndex = arguments.indexOf(KEYWORD_DIRECTORY) + 1;
+
             try {
-                String directory =  arguments.get(directoryIndex);
-                if (directory.contains("\"")) {
-                    for (int i = directoryIndex + 1; i < arguments.size(); i++) {
-                        directory += " " + arguments.get(i);
-                        if (arguments.get(i).contains("\"")) {
-                            break;
-                        }
-                    }
-                    directory = directory.replace("\"", EMPTY_STRING);
-                }
-                directory = directory.replace("/", "\\");
-                return directory;
+                String directory = getFullDirectory(arguments, directoryIndex);
+                return standardiseDirectoryString(directory);
             } catch (IndexOutOfBoundsException e) {
-                return EMPTY_STRING;
+                // No directory was specified
+                return STRING_EMPTY;
             }
+        } else {
+            return STRING_EMPTY;
         }
-        return EMPTY_STRING;
+    }
+
+    /**
+     * Finds the full directory as given in the user's input.
+     * If the user inputs a directory with whitespaces, the user must surround
+     * the directory with double inverted commas as this method will utilise
+     * the presence of double inverted commas to get the full directory.
+     * 
+     * @param arguments
+     * @param directoryIndex
+     */
+    private String getFullDirectory(ArrayList<String> arguments,
+                                    int directoryIndex) throws IndexOutOfBoundsException {
+        String directory = arguments.get(directoryIndex);
+        if (directory.startsWith(STRING_DOUBLE_INVERTED_COMMA)) {
+            for (int i = directoryIndex + 1; i < arguments.size(); i++) {
+                String currentArgument = arguments.get(i);
+                directory += STRING_ONE_SPACE + currentArgument;
+
+                if (currentArgument.endsWith(STRING_DOUBLE_INVERTED_COMMA)) {
+                    break;
+                }
+            }
+            // Remove double inverted commas
+            directory = directory.replace(STRING_DOUBLE_INVERTED_COMMA,
+                                          STRING_EMPTY);
+        }
+        return directory;
+    }
+
+    private String standardiseDirectoryString(String directory) {
+        return directory.replace(STRING_FORWARD_SLASH, STRING_BACKSLASH);
     }
 
     private boolean hasScanCurrentDirOnlyKeyword(ArrayList<String> arguments) {
@@ -136,13 +185,15 @@ public class CommandParser {
         return fileTypes;
     }
 
+    /**
+     * A valid file type is one that is not a keyword.
+     * 
+     * @param inputFileType
+     */
     private boolean isValidFileType(String inputFileType) {
-        String sanitisedFileType = inputFileType.replaceAll("[^a-zA-Z0-9]+",
-                                                            EMPTY_STRING)
-                                                .trim()
-                                                .toLowerCase();
+        String fileType = inputFileType.trim().toLowerCase();
         for (String keyword : KEYWORDS) {
-            if (keyword.equals(sanitisedFileType)) {
+            if (keyword.equals(fileType)) {
                 return false;
             }
         }
@@ -166,18 +217,18 @@ public class CommandParser {
     }
 
     private String findAuthorName(ArrayList<String> arguments) {
-        StringBuilder builder = new StringBuilder();
+        StringJoiner joiner = new StringJoiner(STRING_ONE_SPACE);
         for (String word : arguments) {
-            builder.append(word);
-            builder.append(STRING_ONE_SPACE);
+            joiner.add(word);
         }
-        return builder.toString().trim();
+        return joiner.toString();
     }
 
     
     // ================================================================
     // Create summary command method
     // ================================================================
+   
     private Command initSummaryCommand(ArrayList<String> arguments) {
         return new Command(Command.Type.SUMMARY);
     }
