cc8738a29633775ec22be6643746ccd04964d2fd
Sebastian Quek <sebq13@gmail.com>
2015-07-24 13:47:27 +0800

    FileStats now uses a BorderPane so that its ListView will fill the height of the window.

--- src/main/java/gui/FileStatsController.java
+++ src/main/java/gui/FileStatsController.java
@@ -10,10 +10,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 import main.java.backend.SourceFile;
 
-public class FileStatsController extends VBox {
+/**
+ * This class controls the view that shows an author's individual contributions
+ * to the source files.
+ * 
+ * @author Sebastian Quek
+ *
+ */
+public class FileStatsController extends BorderPane {
 
     @FXML
     private ListView<FileStatsItem> fileStats;
@@ -21,11 +28,12 @@ public class FileStatsController extends VBox {
     @FXML
     private Label title;
 
-    private ArrayList<FileStatsItem> items;
-
     private static final String FILE_STATS_FXML = "/main/resources/layouts/FileStats.fxml";
 
-    public FileStatsController(String authorName, HashMap<SourceFile, Integer> statistics) {
+    private ArrayList<FileStatsItem> items;
+
+    public FileStatsController(String authorName,
+                               HashMap<SourceFile, Integer> statistics) {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(FILE_STATS_FXML));
         loader.setController(this);
         loader.setRoot(this);
@@ -44,18 +52,24 @@ public class FileStatsController extends VBox {
         items = new ArrayList<FileStatsItem>();
         
         for (SourceFile sourceFile : statistics.keySet()) {
-            addFileStatsItem(sourceFile, statistics.get(sourceFile));  
+            addFileStatsItem(sourceFile, statistics.get(sourceFile));
         }
         
         Collections.sort(items);
         fileStats.setItems(FXCollections.observableList(items));
     }
 
+    /**
+     * Each FileStatsItem corresponds to a source file and is displayed as a row
+     * in this custom view.
+     * 
+     * @param currentFile
+     * @param currentNumLines
+     */
     private void addFileStatsItem(SourceFile currentFile, int currentNumLines) {
         items.add(new FileStatsItem(currentFile.getRelativeFilePath(),
                                     currentNumLines,
                                     (double) currentNumLines /
                                             currentFile.getNumLines() * 100));
     }
-
 }
