c9c4b1caf4123c4aeadd078a4ce78f8370c85640
Sebastian Quek <sebq13@gmail.com>
2015-07-30 16:40:26 +0800

    Refactored Collate.java's main method for easier testing of text ui outputs.

--- src/main/java/tui/Collate.java
+++ src/main/java/tui/Collate.java
@@ -1,5 +1,6 @@
 package main.java.tui;
 
+import java.io.PrintStream;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -34,9 +35,9 @@ public class Collate {
     private static final String SUMMARY_HEADER_LOC = "LOC";
     private static final String SUMMARY_HEADER_PROPORTION = "Proportion (%)";
     private static final String SUMMARY_HEADER_DIVIDER =
-        "---------------------------------------------";
-    private static final String FORMAT_SUMMARY_HEADER = "%-15s%10s%20s%n";
-    private static final String FORMAT_SUMMARY_ITEM = "%-15s%10d%20s%n";
+        "---------------------------------------------\n";
+    private static final String FORMAT_SUMMARY_HEADER = "%-15s%10s%20s\n";
+    private static final String FORMAT_SUMMARY_ITEM = "%-15s%10d%20s\n";
 
 
     // ================================================================
@@ -48,9 +49,9 @@ public class Collate {
     private static final String AUTHOR_STATS_HEADER_PROPORTION =
         "Proportion (%)";
     private static final String AUTHOR_STATS_HEADER_DIVIDER =
-        "---------------------------------------------------------------------------";
-    private static final String FORMAT_AUTHOR_STATS_HEADER = "%-45s%10s%20s%n";
-    private static final String FORMAT_AUTHOR_STATS_ITEM = "%-45s%10d%20.2f%n";
+        "---------------------------------------------------------------------------\n";
+    private static final String FORMAT_AUTHOR_STATS_HEADER = "%-45s%10s%20s\n";
+    private static final String FORMAT_AUTHOR_STATS_ITEM = "%-45s%10d%20.2f\n";
 
 
     // ================================================================
@@ -63,6 +64,7 @@ public class Collate {
     private static final int MAX_AUTHOR_NAME_LENGTH = 10;
     private static final int MAX_FILENAME_LENGTH = 40;
 
+    private static boolean isTimeToExit = false;
 
     /**
      * The main method that is executed when this class is called.
@@ -70,39 +72,36 @@ public class Collate {
      * @param args
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        PrintStream output = System.out;
         Logic logic = new Logic();
-        boolean isTimeToExit = false;
 
-        System.out.println(MESSAGE_WELCOME);
+        output.println(MESSAGE_WELCOME);
 
         while (!isTimeToExit) {
-            System.out.print(MESSAGE_COMMAND_PROMPT);
-            String userInput = scanner.nextLine();
-            System.out.println();
-
-            switch (logic.executeCommand(userInput)) {
-                case COLLATE :
-                case SUMMARY :
-                    handleSummaryCommand(logic);
-                    break;
-                case VIEW :
-                    handleViewCommand(logic);
-                    break;
-                case EXIT :
-                    isTimeToExit = true;
-                    System.out.println(MESSAGE_EXIT);
-                    break;
-                case INVALID :
-                default :
-                    handleInvalidCommand();
-                    break;
-            }
-
-            System.out.println();
+            output.print(MESSAGE_COMMAND_PROMPT);
+            String userInput = input.nextLine();
+            output.println();
+            output.println(handleUserInput(logic, userInput));
         }
 
-        scanner.close();
+        input.close();
+    }
+
+    public static String handleUserInput(Logic logic, String userInput) {
+        switch (logic.executeCommand(userInput)) {
+            case COLLATE :
+            case SUMMARY :
+                return handleSummaryCommand(logic);
+            case VIEW :
+                return handleViewCommand(logic);
+            case EXIT :
+                isTimeToExit = true;
+                return MESSAGE_EXIT;
+            case INVALID :
+            default :
+                return handleInvalidCommand();
+        }
     }
 
 
@@ -110,28 +109,29 @@ public class Collate {
     // Methods for summary command
     // ================================================================
 
-    private static void handleSummaryCommand(Logic logic) {
-        showSummaryHeaders();
-        showSummaryRows(logic.getAuthors());
+    private static String handleSummaryCommand(Logic logic) {
+        return getSummaryHeaders() + getSummaryRows(logic.getAuthors());
     }
 
-    private static void showSummaryHeaders() {
-        System.out.format(FORMAT_SUMMARY_HEADER,
-                          SUMMARY_HEADER_AUTHOR,
-                          SUMMARY_HEADER_LOC,
-                          SUMMARY_HEADER_PROPORTION);
-        System.out.println(SUMMARY_HEADER_DIVIDER);
+    private static String getSummaryHeaders() {
+        return String.format(FORMAT_SUMMARY_HEADER,
+                             SUMMARY_HEADER_AUTHOR,
+                             SUMMARY_HEADER_LOC,
+                             SUMMARY_HEADER_PROPORTION) +
+               SUMMARY_HEADER_DIVIDER;
     }
 
-    private static void showSummaryRows(Collection<Author> authors) {
+    private static String getSummaryRows(Collection<Author> authors) {
+        StringBuilder builder = new StringBuilder();
         for (Author author : authors) {
-            System.out.format(FORMAT_SUMMARY_ITEM,
-                              generateTruncatedPhrase(author.getName(),
-                                                      MAX_AUTHOR_NAME_LENGTH,
-                                                      false),
-                              author.getLinesOfCode(),
-                              author.getProportion());
+            builder.append(String.format(FORMAT_SUMMARY_ITEM,
+                                         generateTruncatedPhrase(author.getName(),
+                                                                 MAX_AUTHOR_NAME_LENGTH,
+                                                                 false),
+                                         author.getLinesOfCode(),
+                                         author.getProportion()));
         }
+        return builder.toString();
     }
 
 
@@ -139,36 +139,37 @@ public class Collate {
     // Methods for view command
     // ================================================================
 
-    private static void handleViewCommand(Logic logic) {
+    private static String handleViewCommand(Logic logic) {
         HashMap<SourceFile, Integer> authorStats =
             logic.getTargetAuthorStatistics();
         if (!authorStats.isEmpty()) {
-            showAuthorStatsHeaders();
-            showAuthorStatsRows(authorStats);
+            return showAuthorStatsHeaders() + showAuthorStatsRows(authorStats);
         } else {
-            handleInvalidCommand();
+            return handleInvalidCommand();
         }
     }
 
-    private static void showAuthorStatsHeaders() {
-        System.out.format(FORMAT_AUTHOR_STATS_HEADER,
-                          AUTHOR_STATS_HEADER_FILE_PATH,
-                          AUTHOR_STATS_HEADER_LOC,
-                          AUTHOR_STATS_HEADER_PROPORTION);
-        System.out.println(AUTHOR_STATS_HEADER_DIVIDER);
+    private static String showAuthorStatsHeaders() {
+        return String.format(FORMAT_AUTHOR_STATS_HEADER,
+                             AUTHOR_STATS_HEADER_FILE_PATH,
+                             AUTHOR_STATS_HEADER_LOC,
+                             AUTHOR_STATS_HEADER_PROPORTION) +
+               AUTHOR_STATS_HEADER_DIVIDER;
     }
 
-    private static void showAuthorStatsRows(HashMap<SourceFile, Integer> targetAuthorStatistics) {
+    private static String showAuthorStatsRows(HashMap<SourceFile, Integer> targetAuthorStatistics) {
+        StringBuilder builder = new StringBuilder();
         for (SourceFile file : targetAuthorStatistics.keySet()) {
             int authorLinesOfCode = targetAuthorStatistics.get(file);
-            System.out.format(FORMAT_AUTHOR_STATS_ITEM,
-                              generateTruncatedPhrase(file.getRelativeFilePath(),
-                                                      MAX_FILENAME_LENGTH,
-                                                      true),
-                              authorLinesOfCode,
-                              generatePercentage(authorLinesOfCode,
-                                                 file.getNumLines()));
+            builder.append(String.format(FORMAT_AUTHOR_STATS_ITEM,
+                                         generateTruncatedPhrase(file.getRelativeFilePath(),
+                                                                 MAX_FILENAME_LENGTH,
+                                                                 true),
+                                         authorLinesOfCode,
+                                         generatePercentage(authorLinesOfCode,
+                                                            file.getNumLines())));
         }
+        return builder.toString();
     }
 
     private static String generateTruncatedPhrase(String phrase,
@@ -197,7 +198,7 @@ public class Collate {
     // Methods for invalid command
     // ================================================================
 
-    private static void handleInvalidCommand() {
-        System.out.println(MESSAGE_INVALID_COMMAND);
+    private static String handleInvalidCommand() {
+        return MESSAGE_INVALID_COMMAND;
     }
 }
